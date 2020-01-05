8528ba13fbb917da7172bb803538462705b99112
WangYu9793 <1141288616@qq.com>
2020-01-05 18:57:46 +0800

    test

---
+++ 202015/202015/Source.c
@@ -0,0 +1,12 @@
+public class Solution {
+	public int jumpFloor(int number) {
+		if (number <= 0)
+			return 0;
+		else if (number == 1)
+			return 1;
+		else if (number == 2)
+			return 2;
+		else
+			return jumpFloor(number - 1) + jumpFloor(number - 2);
+	}
+}
