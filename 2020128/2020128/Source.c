8e000a5c22184418dcc23b4b2d5f28e4957178d5
WangYu9793 <1141288616@qq.com>
2020-01-28 20:59:00 +0800

    test

---
+++ 2020128/2020128/Source.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int x = 1, y = 0, a = 0, b = 0;
+	switch (x)
+	{
+	case 1:switch (y)
+	{
+		case
+			0:a++; break;
+			case
+				1:b++; break;
+	}
+	case 2:a++; b++; break;
+	case 3:a++; b++; break;
+	}
+	printf("a=%d,b=%d\n", a, b);
+	system("pause");
+	return 0;
+}
